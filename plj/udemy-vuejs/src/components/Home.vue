06ae4f115dc16373c807d8497dcecd3ffc5bdf5e
Guppi334 <st0910na0401@gmail.com>
2020-06-18 10:55:06 +0900

    add plj

---
+++ plj/udemy-vuejs/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>Home</p>
+        <input type="text">
+    </div>
+</template>
+
+<script>
+export default {
+    destroyed() {
+        console.log("destroyed Home");
+    },
+    deactivated() {
+        console.log("deactivated Home");
+    },
+    activated() {
+        console.log("activated Home")
+    }
+}
+</script>
---
+++ plj/udemy-vuejs/src/components/LikeHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <hr>
+        <h2>いいねの数</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            user: {
+            firstName: "Jack",
+            lastName: "Donald"
+            },
+            search: "searchText"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
