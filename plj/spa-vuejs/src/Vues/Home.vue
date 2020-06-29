46cae5d9acf2f3d1b6043264eee8f3a6433d3dcc
Guppi334 <st0910na0401@gmail.com>
2020-06-29 09:32:31 +0900

    finish studying publish appulication to netlify

--- plj/spa-vuejs/src/Vues/Home.vue
+++ plj/spa-vuejs/src/Vues/Home.vue
@@ -1,44 +1,45 @@
 <template>
-    <div>
-        <h3>Home</h3>
-        <button @click="toUsers">Usersページに行く</button>
-        <p>{{ doubleCount }}</p>
-        <p>{{ tripleCount }}</p>
-        <input type="text" v-model="message">
-        <p>{{message}}</p>
-    </div>
+  <div>
+    <h3>Home</h3>
+    <button @click="toUsers">Usersページに行く</button>
+    <p>{{ doubleCount }}</p>
+    <p>{{ tripleCount }}</p>
+    <input type="text" v-model="message" />
+    <p>{{message}}</p>
+  </div>
 </template>
 
 <script>
 // import { mapGetters } from 'vuex';
 
 export default {
-    methods: {
-        toUsers() {
-            this.$router.push({
-                name: 'users-id-profile', 
-                params: { id: 1}});
-        }
+  methods: {
+    toUsers() {
+      this.$router.push({
+        name: "users-id-profile",
+        params: { id: 1 }
+      });
+    }
+  },
+  computed: {
+    // ...mapGetters("count",["doubleCount","tripleCount"]),
+    doubleCount() {
+      return this.$store.getters["count/doubleCount"];
     },
-    computed: {
-        // ...mapGetters("count",["doubleCount","tripleCount"]),
-        doubleCount() {
-            return this.$store.getters["count/doubleCount"]
-        },
-        tripleCount() {
-            return this.$store.getters["count/tripleCount"]
-        },
-        // message() {
-        //     return this.$store.getters.message;
-        // }
-        message: {
-            get() {
-                return this.$store.getters.message;
-            },
-            set(value) {
-                this.$store.dispatch("updateMessage", value)
-            }
-        }
-    }, 
-}
+    tripleCount() {
+      return this.$store.getters["count/tripleCount"];
+    },
+    // message() {
+    //     return this.$store.getters.message;
+    // }
+    message: {
+      get() {
+        return this.$store.getters.message;
+      },
+      set(value) {
+        this.$store.dispatch("updateMessage", value);
+      }
+    }
+  }
+};
 </script>
